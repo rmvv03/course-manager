4dee90194906b949bfcd6086965eae3477cb061a
rmvv03 <mateovv03@gmail.com>
2026-07-21 16:18:51 -0600

    Add files via upload

---
+++ index-C3eUm10b.js
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Course Tracker and Weekly Planner</title>
+    <script type="module" crossorigin src="./assets/index-C3eUm10b.js"></script>
+    <link rel="stylesheet" crossorigin href="./assets/index-o2-93u3u.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
